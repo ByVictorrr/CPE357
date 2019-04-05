bd6ee2b8166b98233688ce75b5de59567d1066b0
byvictorrr <vdelaplaines@aol.com>
2019-04-05 20:08:37 +0000

    added

---
+++ HW/HW1/mytr.c
@@ -0,0 +1,17 @@
+/*
+ * mytr.c
+ * Copyright (C) 2019 vdelaplainess <vdelaplainess@arch>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "mytr.h"
+#include <stdio.h>
+
+int getline(char input[], int size);
+
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
