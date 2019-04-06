7d02b4a6dd41b18591c3f80ab9b40648dd6601ea
byvictorrr <vdelaplaines@aol.com>
2019-04-06 08:47:01 +0000

    added

--- HW/HW1/mytr.c
+++ HW/HW1/mytr.c
@@ -7,11 +7,27 @@
 
 #include "mytr.h"
 #include <stdio.h>
+#define 10000 /*safety of space */
 
-int getline(char input[], int size);
+char [MAX] input;
+int getline(char *input, int size);
 
-
+//compare argv[0](SET ONE) and argv[1] (SET TWO)
+//have user keep enetering values
+// if (int size1 = sizeof(set1)/(sizeof(int)
 int main(int argc, char** argv)
 {
+	switch () //switch flag maybe?
 	return 0;
+}
+
+/* reading a string using stdin */
+int getline(char *input)
+{
+	int c, i;
+	i = 0;
+	while ( (c=getchar()) != EOF){
+		input[i] = c;
+		i++;
+	}
 }
