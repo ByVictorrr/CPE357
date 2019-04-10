31ffdde8ed33547bf41e336d08833f1b7e2392ca
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-09 19:05:02 -0700

    autoadd

--- hw/hw1/tests/testT/mapStringToChar.c
+++ hw/hw1/tests/testT/mapStringToChar.c
@@ -5,7 +5,6 @@
 
 char line[MAXCHAR]; /*deleteChar: for -d flag, deletes each char in set, that is found in line
  */
-char *dupPtr = NULL;
 
 int strl(char * s)
 {
@@ -36,44 +35,6 @@ int mapChar(char *line, char c1, char c2)
 }                    
 
 
-/* getDup: returns a pointer to space allocated in heap, if there are duplicates*/
-char *getDup(char *set1)
-{
-	int size_heap = strl(set1)+1 ; /*plus one to include \0*/
-	char *dupPtr = (char*)calloc(size_heap, sizeof(char)); /* allocates MAXCHAR/2 elements in heap contingously) */
-	printf("\n max allocated: size of :  %d", size_heap);
-	return dupPtr;
-}
-
-/*charDup: checks if there is a duplicate of a character in set1
- * returns 1 - if there is a duplicate of a char in set1
- * return 0 - if there is no duplicate of a char in set1
- * Alters ptrDup - if duplicate found, if multiple of the same char
- *				 - changes ptrDup to have n-1 of multiple chars found in line
-*/
-int charDup(char *set1)
-{
-	int i, j;
-	int k=0; /*indicate how many duplicates, alse used to incrment char array*/
-
-	for(i = 0; i< strl(set1); i++)
-		for (j = i+1;  j < strl(set1); j++)
-			if ( set1[i] == set1[j]){
-				if (++k==1 && dupPtr == NULL) /* if at least one duplicate create memory*/	
-				{
-					dupPtr = getDup(set1);
-					dupPtr[k-1] = set1[j]; 
-				}
-				else if (dupPtr != NULL)
-					dupPtr[k-1] = set1[j]; /* duplicate*/
-			}
-	dupPtr[k+1] = '\0'; /* to tell in the heap when the last value is*/
-	if (k>0)
-		return 1; /* if at least one dup*/
-	return 0;
-}
-
-
 /*cmpStr: compares length of set1 and set2
  * return -1 - if set1 < set2
  * return 0 - if set1==set2
@@ -81,7 +42,7 @@ int charDup(char *set1)
  */
 int cmpStr(char *set1, char *set2)
 {
-	if(strl(set1) > strl(set2)
+	if(strl(set1) > strl(set2))
 			return 1;
 	else if (strl(set1) == strl(set2))
 			return 0;
@@ -93,18 +54,26 @@ int cmpStr(char *set1, char *set2)
 
 int mapStringToChar(char *line, char *lastChars, char c2 )
 {
-	int i,j;
-	char temp;
+	int i, j;
+	int startIndex = 0;	
 
-	for ( i=0, j=0; i< strl(line); i++) /*plus one because of null char not recognized by strl*/
+	for ( i=0, j=0; i< strl(line)+1; i++) /*plus one because of null char not recognized by strl*/
 	{
-		if (line[i] == *lastChars){
-			temp = *lastChars++;
-			j++;
+		if(j > 1 && *lastChars == '\0'){ /* if the we found lastchars[] in line replace it in fist index that was fou*/
+			line[startIndex] = c2; /* finally map c2 to string*/
+			line[startIndex+1] = '\0';
 		}
-		if(j > 2) /* if the line has "lastchar[i] .... in it then
+		else if (line[i] == *lastChars){
+			lastChars++;
+			if( j==0){
+				startIndex = i; /*get index of starting index */
+				j++;
+			}else
+				j++;
 			
-	}
+		}
+		else
+			j=0; /*indicating need to start over searching string*/
 	}
 	if(j == 0)
 		return 0; /*c1 isnt in line*/
@@ -116,12 +85,14 @@ int main()
 {	line[0] = 'a';
 	line[1] = 'h';
 	line[2] = 'i';
-	line[3] = 'h';
-	line[4] = '\0';
+	line[3] = 'c';
+	line[4] = 't';
+	line[5] = 'o';
+	line[6] = 'r';
+	line[7] = '\0';
 	
-
-	char *set1 = "hihhdfi";
-	char *set2 = "kay";	
+	char *set1 = "victor";
+	char *set2 = "kay";
 	int size_set1 = strl(set1);
 	int size_set2 = strl(set2);
 	int i = 0;
@@ -137,11 +108,15 @@ int main()
 		 * then it maps {set1[strl(set2)-1], ...,set1[strl(set1)-1]} -> set2[strl(set2)-1];
 		 */
 		
-		while (size_set2-- > 0 && size-- > 0 ) /*not at last value of set2*/
+		printf("line before mapChar: %s ",line);
+		while (--size_set2 > 0 ) /*not at last value of set2*/
 				i+=mapChar(line, *set1++, *set2++);
 		/*when last value in set2*/
 		/*mapStringToChar(line, char *set(pointing to address of last index of set2), c2)*/
 		
+		printf("\nline before StringToChar: %s ",line);
+		i+=mapStringToChar(line, set1, *set2);
+		printf("\nline changed: %s ",line);
 	}
 
 	else{ /* if set1 is less than or equal to set2 lenght*/
@@ -152,16 +127,6 @@ int main()
 		}
 
 
-
-
-	if (charDup(set1) > 0)
-	{
-		printf("\nshould be 1 for duplicates: %d \n", 1);
-		printf("\nptr = shoudl equal h: %s", dupPtr);
-		free(dupPtr);
-	}
-	else
-		printf("should be 0 for no duplicates: %d \n", 0);
 
 
 	return 0;
