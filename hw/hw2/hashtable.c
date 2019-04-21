0d1b30863e1af959b8f54ac23b43425870517977
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-21 14:05:06 -0700

    autoadd

--- hw/hw2/hashtable.c
+++ hw/hw2/hashtable.c
@@ -6,9 +6,8 @@
 /*entry in the hash table*/
 struct node{
 
-	int key;
-	int wCount;
-	char *word;
+	int wCount; //value
+	char *word; //key
 	struct node * next;
 };
 
@@ -61,10 +60,44 @@ hashtable *initalizeTable(int size)
 	return table;
 }
 
-int insertNode()
-int hash(hash_table *table, int key) 
+/*insertNode: returns the index at which node inserted*/
+int insertNode(hashtable *table, char *word)
 {
-	return key % table->size;
+	//step1 - table is empty
+	if(table == NULL)
+		return -1;
+	
+	int index = hash(table, word); //index into table
+
+	node *entry = hash->buckets[i]; //get the ith index in the table
+
+	//step 2 - if entry isnt null that means there is a entry in there
+	while (entry != NULL)
+	{
+		//Step 2.1 - check if the word is in the table alread 
+		if(entry->word == word) //if the same word lets incrment wCount
+		{
+			entry->wCount++;
+			return index; //if same word return that index
+		}
+
+		entry = entry->next; //go to the next node
+	}
+	//Step 2.2 - this means the two enteries share same hash value (key) but arent same word
+	entry = (node*)malloc(sizeof(node)); //creat a new node
+	entry->wCount = 1;
+	entry->word = (char*)malloc(strlen(word));
+	
+	return index;
+}
+
+int hash(hash_table *table, char *word) 
+{
+	int i, hash;
+	for(i=0; i<table->size; i++)
+		hash = 31*hash + key[i];
+
+	return hash % table->size;
 }
 
 
