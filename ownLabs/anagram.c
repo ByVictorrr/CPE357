b14c49c6b4dc89cd4ba09fe7268d543e6720db2d
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-23 03:05:07 -0700

    autoadd

---
+++ ownLabs/anagram.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *anagram(char *s)
+{
+	char *temp = s;
+	while(*temp)
+		temp++;
+	int size = temp - s;
+	int i;
+	for(i=size; i>0; i++)
+		*temp++ = s[i-rand()%size];
+
+	return s;
+}
+
+int main()
+{
+	
+	printf("%s",anagram("anagram"));
+		
+		
+		
+	return 0;
+}
