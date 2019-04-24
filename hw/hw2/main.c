f1591b5d1659781caa9207b434d2201fd064efe7
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-23 23:25:21 -0700

    autoadd

--- hw/hw2/main.c
+++ hw/hw2/main.c
@@ -5,113 +5,6 @@
 #include "fw.h"
 #define SIZE 100
 
-
-int main(int argc, char *argv[])
-{
-	struct node *addToAddrNodeArr(struct node *root, int numOfNodes);
-	int TopWords = 10;
-	FILE *fp;
-	int arraysize = 40;
-	char *word = (char *)malloc(arraysize * sizeof(char));
-	int wordsize = 0;
-	struct node *root = NULL;
-	struct node *ptrArry;
-
-	if (fp == NULL)
-		switch (argc)
-		{
-	case 1: /*case 1: only input executable*/
-			/*this case we want to get input from std in*/
-			while (wordsize != -1)
-			{
-				wordsize = bufferinput(stdin, word, &arraysize);
-			}
-
-			break;
-			/*==============================================================================*/
-		case 2: /*could use option */
-			/*case 2.1: if the argv[1] != -n treat is a file input*/
-			if (!strcmp(argv[1], "-n"))
-				fprintf(stderr, "usage: fw [-n num] [ file1 [ file2 [..]]]\n");
-			/*case 2.2: if the argv[1] is anything other than -n treat it as a file**/
-			else
-			{
-
-				if ((fp = fopen(argv[1], 'r')))
-				{
-					while (wordsize != -1)
-					{
-						wordsize = bufferinput(fp, word, &arraysize);
-						root = insertNode(root, word);
-					}
-
-				}
-				else /*else file ptr null - something wrong with the file*/
-					fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
-				
-				
-				ptrArry = addToAddrNodeArr(root,numOfNodes);
-				mergeSort(ptrArry,0,numOfNodes-1);
-				
-				break;
-			}
-			/*==============================================================================*/
-		case 3: /*case 3: could be -n option and number, or just files or combination*/
-			/*case 3.1: fw -n number*/
-			if (!strcmp(argv[1], "-n") && isdigit(argv[2]))
-			{
-				TopWords = argv[2];
-				while (wordsize != -1)
-				{
-					wordsize = bufferinput(stdin, word, &arraysize);
-					root = insertNode(root, word);
-				} /*stuff read in now need to do stuff*/
-				ptrArry = addToAddrNodeArr(root,numOfNodes);
-				mergeSort(ptrArry,0,numOfNodes-1);
-			}
-			else
-			{
-				fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
-			}
-
-		default:
-
-			if (!strcmp(argv[1], "-n") && isdigit(argv[2]))
-			{
-				TopWords = argv[2];
-				while (wordsize != -1)
-				{
-					wordsize = bufferinput(fp, word, &arraysize);
-					root = insertNode(root, word);
-				}
-				ptrArry = addToAddrNodeArr(root,numOfNodes);
-				mergeSort(ptrArry,0,numOfNodes-1);
-			}
-			else
-			{
-				int i = 1;
-				while (i < argc)
-				{
-					if (fp = fopen(argv[i], 'r'))
-					{
-						while (wordsize != -1)
-						{
-							wordsize = bufferinput(fp, word, &arraysize);
-							root = insertNode(root, word);
-						}
-					}else{
-						fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
-					}
-				}
-				ptrArry = addToAddrNodeArr(root,numOfNodes);
-				mergeSort(ptrArry,0,numOfNodes-1);
-			}
-		}
-
-	return 0;
-}
-
-
 void readOutWords(int amount, struct node *arr[])
 {
     int n = 0;
@@ -168,7 +61,21 @@ void merge(struct node *arr[], int left, int middle, int right)
     }
 }
 
-    /* Copy the remaining elements of L[], if there */
+ /* left is initally 0 and right is n-1 for the size */
+void mergeSort(struct node arr[], int left, int right)
+{
+    if (left < right)
+    {
+        // this is the better way to do it.. no segfault
+        int middi = left + (right - left) / 2;
+
+        // Sort first then the second...the dirty dirty second
+        mergeSort(arr, left, middi);
+        mergeSort(arr, middi + 1, right);
+
+        merge(arr, left, middi, right);
+    }
+}   /* Copy the remaining elements of L[], if there */
 
 int bufferinput(FILE *fp, char *word, int *arraylength)
 {
@@ -309,6 +216,113 @@ struct node *searchNode(struct node *root, char *word )
 		return searchNode(root->left_child, word);
 	else 
 		return searchNode(root->right_child,word);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	struct node *addToAddrNodeArr(struct node *root, int numOfNodes);
+	int TopWords = 10;
+	FILE *fp;
+	int arraysize = 40;
+	char *word = (char *)malloc(arraysize * sizeof(char));
+	int wordsize = 0;
+	struct node *root = NULL;
+	struct node *ptrArry;
+
+	if (fp == NULL)
+		switch (argc)
+		{
+	case 1: /*case 1: only input executable*/
+			/*this case we want to get input from std in*/
+			while (wordsize != -1)
+			{
+				wordsize = bufferinput(stdin, word, &arraysize);
+			}
+
+			break;
+			/*==============================================================================*/
+		case 2: /*could use option */
+			/*case 2.1: if the argv[1] != -n treat is a file input*/
+			if (!strcmp(argv[1], "-n"))
+				fprintf(stderr, "usage: fw [-n num] [ file1 [ file2 [..]]]\n");
+			/*case 2.2: if the argv[1] is anything other than -n treat it as a file**/
+			else
+			{
+
+				if ((fp = fopen(argv[1], 'r')))
+				{
+					while (wordsize != -1)
+					{
+						wordsize = bufferinput(fp, word, &arraysize);
+						root = insertNode(root, word);
+					}
+
+				}
+				else /*else file ptr null - something wrong with the file*/
+					fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
+				
+				
+				ptrArry = addToAddrNodeArr(root,numOfNodes);
+				mergeSort(ptrArry,0,numOfNodes-1);
+				
+				break;
+			}
+			/*==============================================================================*/
+		case 3: /*case 3: could be -n option and number, or just files or combination*/
+			/*case 3.1: fw -n number*/
+			if (!strcmp(argv[1], "-n") && isdigit(argv[2]))
+			{
+				TopWords = argv[2];
+				while (wordsize != -1)
+				{
+					wordsize = bufferinput(stdin, word, &arraysize);
+					root = insertNode(root, word);
+				} /*stuff read in now need to do stuff*/
+				ptrArry = addToAddrNodeArr(root,numOfNodes);
+				mergeSort(ptrArry,0,numOfNodes-1);
+			}
+			else
+			{
+				fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
+			}
+
+		default:
+
+			if (!strcmp(argv[1], "-n") && isdigit(argv[2]))
+			{
+				TopWords = argv[2];
+				while (wordsize != -1)
+				{
+					wordsize = bufferinput(fp, word, &arraysize);
+					root = insertNode(root, word);
+				}
+				ptrArry = addToAddrNodeArr(root,numOfNodes);
+				mergeSort(ptrArry,0,numOfNodes-1);
+			}
+			else
+			{
+				int i = 1;
+				while (i < argc)
+				{
+					if (fp = fopen(argv[i], 'r'))
+					{
+						while (wordsize != -1)
+						{
+							wordsize = bufferinput(fp, word, &arraysize);
+							root = insertNode(root, word);
+						}
+					}else{
+						fprintf(stderr, "usage No such file or directory\n The top 10 words (out of 0 are:\n");
+					}
+				}
+				ptrArry = addToAddrNodeArr(root,numOfNodes);
+				mergeSort(ptrArry,0,numOfNodes-1);
+			}
+		}
+
+	return 0;
 }
 
 
