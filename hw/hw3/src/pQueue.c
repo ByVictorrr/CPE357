17c25ee8449d94a821b2531774bffb61d9b3d2b3
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-29 19:11:44 -0700

    autoadd

--- hw/hw3/src/pQueue.c
+++ hw/hw3/src/pQueue.c
@@ -56,20 +56,33 @@ void pushNew(listNode **head, char c, int freq) {
         return;
     }
 
-    /*if freq is less insert at the head*/
+	/*case 2 - the data to be inserted has less freq than the head*/
     if(beg->curr->freq > freq)
     {
         tempNode->next= *head;
         *head = tempNode;
-
     }
+	/*case 3 - general case (it goes through the list and finds a spot)*/
     else
     {
         /*find position where it belongs*/
         while ( beg->next != NULL && beg->next->curr->freq < freq)
         {
-            beg = beg->next;
+		   	beg = beg->next;
         }
+
+		/* there could be equal freq, so prioritze it by 2nd field*/
+		if( beg->next !=NULL &&  beg->next->curr->freq == freq)
+		{
+			/*case 3.1 - sort by character ascii code if have equal freq*/
+			while(beg->next !=NULL &&  beg->next->curr->freq == freq)
+			{
+				
+			}
+
+		}
+
+		/*At this poostion soon to inserted freq should be less than or equal to beg*/
         tempNode->next = beg->next;
         beg->next = tempNode;
 
@@ -83,20 +96,22 @@ void pushNode(listNode **head, listNode *parent)
     /*new node*/
     listNode *tempNode;
 
-    /*if freq is less insert at the head*/
+	/*check to see if we can insert at the head*/
     if(beg->curr->freq > parent->curr->freq)
     {
         parent->next= *head;
         *head = parent;
 
     }
-    else
-    {
+    
+	/*else if its frequenc isnt less than the head*/
         /*find position where it belongs*/
         while ( beg->next != NULL && beg->next->curr->freq < parent->curr->freq )
         {
             beg = beg->next;
         }
+		/*At this point parent-curr->freq is less than beg->next-curr->breq or equal to*/
+		if (beg->curr->
         parent->next = beg->next;
         beg->next = parent;
 
@@ -146,14 +161,14 @@ void transverse(listNode *head)
         head=head->next;
     }
 }
-/*
+
 
 int main()
 {
 
     listNode *head = newListNode('a', 1);
     push(&head, 'a', 2);
-    push(&head, 'b', 2);
+    push(&head, 'b', 1);
     printf("list");
 
     while(!isEmpty(&head))
