cbf27a0c46a3926905d0fd6754de99820250b70e
Victor Delaplaine <vdelaplaines@aol.com>
2019-05-04 18:52:33 -0700

    autoadd

---
+++ hw/hw3/src/getExpected.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for file in $( ls inputs/* ); do
+	 echo -n "Testing with ${file} -- "
+	 BASE=`basename ${file} .in`
+	 #program testing
+
+	 ~pn-cs357/demos/./hencode ${file} expected/${BASE}.expected
+
+	 if [ $? -eq 0 ]; then
+		 echo "succeeded"
+
+	 fi
+	 done
+
+
+
