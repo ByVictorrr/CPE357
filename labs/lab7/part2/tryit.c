ef9d603116daedd392c2b2058e08c9f2528c6bac
Victor Delaplaine <vdelaplainess@gmail.com>
2019-05-20 16:08:33 -0700

    Update tryit.c

--- labs/lab7/part2/tryit.c
+++ labs/lab7/part2/tryit.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-
-
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
 int main(int argc, char *argv[])
 {
+	pid_t pid;
+	int status;
 
+	if((pid = fork()) < 0){
+			perror("fork err");
+			exit(EXIT_FAILURE);
+	}else if( pid == 0){ /*child*/
+		if(execl(argv[1],argv[1], (char *)0)  < 0){
+			perror(argv[1]);	
+			exit(3);
+		}
+		printf("Process %d suceeded", pid);
+	}else{ /*parent*/
+		wait(&status);
+		if(WIFEXITED(status))
+			printf("Process %d exited with an error value\n", pid);
 
-	return 0;
+	}
+
+	exit(0);
 }
