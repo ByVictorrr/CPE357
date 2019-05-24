536bf5bf7fce6af132cd6baa6c2785e374a2dda4
Chenxi Xiao <chenxixiao@Chenxis-MacBook-Air.local>
2019-05-24 00:17:59 -0700

    ..

--- hw/hw4/mytar/table.c
+++ hw/hw4/mytar/table.c
@@ -2,6 +2,25 @@
 #include "header.h"
 #include "creat.h"
 
+static unsigned int OctToInt(const char* str, unsigned int len) {
+	unsigned int res = 0;
+	unsigned int i;
+	for (i = 0; i < len; i++) {
+		char c = str[i];
+		if (c){
+            res *= 8;
+		    res += c - '0';
+        }
+        else
+        {
+            break;
+        }
+	}
+	return res;
+}
+
+
+
 int topStrContains(char *base, char *key) {
     int keyLen = strlen(key);
     char _base[keyLen + 1];
@@ -15,12 +34,14 @@ int topStrContains(char *base, char *key) {
     ret = strcmp(_base, _key);
     return ret;
 }
-void print_verbose(headerEntry header){
-	const mode_t mode = oct2uint (header -> mode, 7);
-	const char str
+void print_table(headerEntry header, int verbose){
+	const mode_t mode = OctToInt (header -> mode, 7);
+	const char str, ownergr[20];
+	struct tm * time;
+	time_t mtime;
 	/*test*/
 	mode = 777;
-	mode = { "-hlcbdp-"[entry -> typeflag? entry -> typeflag -'0':0],
+	str = { "-hlcbdp-"[entry -> typeflag? entry -> typeflag -'0':0],
 						mode & S_IRUSR?'r': '-',
 						mode & S_IWUSR?'w':'-',
 						mode & S_IXUSR?'x':'-',
@@ -32,67 +53,90 @@ void print_verbose(headerEntry header){
 						mode & S_IXOTH?'x':'-',
 						' ',
 						'\0'};
+	/* Writing Permission 10 Owner/Group 17 */
+	strcpy(ownergr, entry -> uname);
+	strcat(ownergr, entry-> gname);
+	printf("%s %-17s", str, ownergr);	
+	switch (entry -> type){
+	case REGULAR: case NORMAL: case CONTIGUOUS:
+		printf("%+8d",entry -> size);
+		break;
+	case HARDLINK: case SYMLINK: case DIRECTORY: case FIFO:
+		printf("%+8d",entry -> size);
+		break;
+	case CHAR: case BLOCK:
+		printf( "%d,%d", entry -> major, entry -> minor);
+		break;
+	default: 
+		fprintf(stderr, "Error: failed to list archive in verbose mode");
+	}
 
-	 printf("%s", str);	
+	mtime = OctToInt(entry -> mtime);
+	time = localtime(&mtime);
+	printf(" %4d-%02d-%02d %02d:%02d ", time -> tm_year + 1900, time -> tm_mon + 1, time -> tm_mday, time -> tm_hour, time -> tm_min);
+	}
+
+	fprintf(f, "%s", entry -> name);
+
+	if (verbose){
+		switch (entry -> type){
+			case HARDLINK:
+				printf(" link to %s", entry -> link_name);
+				break;
+			case SYMLINK:
+				printf(" -> %s", entry -> link_name);
+				break;
+			break;
+		}
+	}
 }
-void print_table(char* pathname, int verbose){
+void list_archieve(char* pathname, int verbose){
 	struct dirent *entry;
 	struct headerEntry header;
-	struct stat curr_file_info;
+	struct stat st;
 	DIR *dp;
 	
 	if ((dp = opendir(pathname)) == NULL) {
     	perror("-t print: unable to open dir");
     	exit(EXIT_FAILURE);
     }
-
+	chdir(pathname);
 	while ((entry = readdir(dp)) != NULL) {
     	if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
         	continue;
     	} 
+		else{
+			if (lstat(entry->d_name), &st) == -1) {
+					perror("traverse");
+					exit(EXIT_FAILURE);
+				}
+				if (S_ISDIR(st.st_mode) == 0) {
+					get_stats(entry->d_name, &header);
+					print_table(header, verbose);
+					strcat(pathname, "/");
+					list_archieve(pathname, verbose);
+				}
+				else if (S_ISREG(curr.st_mode) == 0) {
+					/*strcat(pathname, entry->d_name);*/
+					/*print path name if verbose is used*/
+					get_stats(pathname, &header);
+					print_table(header, verbose);
+				}
+			else if (S_ISLNK(curr.st_mode)) {
+				/*strcat(pathname, entry->d_name);*/
+				/*print path name if verbose is used*/
+				if (flags == 0 || flags == 1) {
+					printf("%s\n", pathname);
+				}
 
-    	if (lstat(strcat(pathname, entry->d_name), &curr) == -1) {
-			perror("traverse");
-			exit(EXIT_FAILURE);
-		}
-
-		if (S_ISDIR(curr.st_mode)) {
-
-			get_stats(pathname, &header)
-			if(verbose){
-				print_verbose(header);
+				add_archive_entry(pathname, entry->d_name, outfd, 1, flags);
+				end = pathname + strlen(pathname) - (strlen(entry->d_name));
+				*end = '\0';
 			}
-			
-        	printf("%s\n", pathname);
-        
-
-			strcat(pathname, "/");
-			
-			print_table(entry->d_name, verbose);
+		
 		}
-      else if (S_ISREG(curr.st_mode)) {
-         /*strcat(pathname, entry->d_name);*/
-         /*print path name if verbose is used*/
-         if (flags == 0 || flags == 1) {
-            printf("%s\n", pathname);
-         }
-
-         add_archive_entry(pathname, entry->d_name, outfd, 0, flags);
-         end = pathname + strlen(pathname) - (strlen(entry->d_name));
-         *end = '\0';
-      }
-      else if (S_ISLNK(curr.st_mode)) {
-         /*strcat(pathname, entry->d_name);*/
-         /*print path name if verbose is used*/
-         if (flags == 0 || flags == 1) {
-            printf("%s\n", pathname);
-         }
-
-         add_archive_entry(pathname, entry->d_name, outfd, 1, flags);
-         end = pathname + strlen(pathname) - (strlen(entry->d_name));
-         *end = '\0';
-      }
-   }
+	}
+    
 }
 
 void Table(int* flags, int argc, char **argv){
@@ -112,7 +156,7 @@ void Table(int* flags, int argc, char **argv){
 		if (argc > MIN_ARGS) {
             for (i = MIN_ARGS; i < argc; i++) {
                 if (topStrContains(&name, argv[i]) == 0) {
-                    __listArchive(&name, &header, verbose);
+                    list_archieve(char* pathname, int verbose)
                 }
 				else{
 					fprintf(stderr, "Mytar: %s: Not found in archive\nMytar: Error exit delayed from previous errors.", argv[i]);
@@ -121,7 +165,7 @@ void Table(int* flags, int argc, char **argv){
             }
         } 
 		else {
-            __listArchive(&name, &header, verbose);
+            list_archieve(char* pathname, int verbose)
         }
 
         if (header.typeflag == REGTYPE) {
@@ -132,60 +176,4 @@ void Table(int* flags, int argc, char **argv){
         }
     }
       
-	
-if(flags[V]){
-    struct stat info;
-    if (stat(fp, &) == -1) {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
-
-    
-	/*User*/
-	if (S_IRUSR & info.st_mode)
-		access[1] = 'r';
-	else
-		access[1] = '-';
-
-	if (S_IWUSR & info.st_mode)
-		access[2] = 'w';
-	else
-		access[2] = '-';
-
-	if (S_IXUSR & info.st_mode)
-		access[3] = 'x';
-	else
-		access[3] = '-';
-
-
-	/*group*/
-	if(S_IRGRP & info.st_mode )
-		access[4] = 'r';
-	else
-		access[4] = '-';
-
-	if (S_IWGRP & info.st_mode )
-		access[5] = 'w';
-	else
-		access[5] = '-';
-
-	if(S_IXGRP & info.st_mode )
-		access[6] = 'x';
-	else
-		access[6] = '-';
-
-	/*owner*/
-	if(S_IROTH & info.st_mode )
-		access[7] = 'r';
-	else
-		access[7] = '-';
-	if(S_IWOTH & info.st_mode )
-		access[8] = 'w';
-	else
-		access[8] = '-';
-	if(S_IXOTH & info.st_mode )
-		access[9] = 'x';
-	else
-		access[9] = '-';
-	
 }
