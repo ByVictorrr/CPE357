982dfb0785ad3893088688931c325ece3cdf2969
Chenxi Xiao <chenxixiao@Chenxis-MacBook-Air.local>
2019-05-26 14:25:34 -0700

    ..updated my table.c

--- hw/hw4/mytar/table.c
+++ hw/hw4/mytar/table.c
@@ -24,6 +24,48 @@ static unsigned int OctToInt(const char *str, unsigned int len)
 	return res;
 }
 
+uint32_t extract_special_int(char *where, int len) {
+/* For interoperability with GNU tar. GNU seems to
+* set the high–order bit of the first byte, then
+* treat the rest of the field as a binary integer
+* in network byte order.
+* I don’t know for sure if it’s a 32 or 64–bit int, but for * this version, we’ll only support 32. (well, 31)
+* returns the integer on success, –1 on failure.
+* In spite of the name of htonl(), it converts int32 t */
+	int32_t val= -1;
+	if ( (len >= sizeof(val)) && (where[0] & 0x80)) {
+	/* the top bit is set and we have space * extract the last four bytes */
+	val = (int32_t)(*(where + len - sizeof(val)));
+	val = ntohl(val); /* convert to host byte order */ }
+	return val; 
+}
+
+
+unsigned long extract_octal(char *where, int len, int strict) {
+/* extract an octal value that is either nul– or space–terminated 320
+       * increments *err on error.
+*/
+	char tmpstring[513]; /* used for numeric conversions */ 
+	char *end;
+	long num;/* make a string that we’re sure is nul–terminated */ 
+	memcpy(tmpstring, where, len);
+	tmpstring[len] = '\0';
+	/* convert the number */
+	num = strtol(tmpstring, &end, 8);
+	if ( *end !='\0'&& *end !=' ') {
+		if ( strict  ) 
+				perror("strict");
+		else {
+	/* see if it’s an encoded int like GNU does */ 
+	num = extract_special_int(where, len);
+			if ( num < 0 )
+					perror("extract octal");
+		} 
+	}
+	return num; 
+}
+
+
 void print_table(headerEntry *header, int verbose)
 {
 	const mode_t mode = OctToInt((char *)header->mode, 7);
@@ -53,14 +95,14 @@ void print_table(headerEntry *header, int verbose)
 		strcpy(ownergr, (char *)header->uname);
 		strcat(ownergr, (char *)header->gname);
 		printf("%s %-17s", str, ownergr);
-		printf("%+8s", (char *)header->size);
+		printf("%8d", (int)extract_octal((char *)header -> size, 11, 0));
+		mtime = extract_octal((char *)header -> mtime, 11, 0);
 		/*mtime = OctToInt(header -> mtime, 11);*/
-		mtime = header->mtime;
 		time = localtime(&mtime);
 		printf(" %4d-%02d-%02d %02d:%02d ", time->tm_year + 1900, time->tm_mon + 1, time->tm_mday, time->tm_hour, time->tm_min);
 	}
 
-	printf("%s\n", header->name);
+	printf("%s", header->name);
 
 	if (verbose)
 	{
@@ -69,6 +111,7 @@ void print_table(headerEntry *header, int verbose)
 			printf(" -> %s", header->linkname);
 		}
 	}
+	printf("\n");
 }
 int makeBlockSize(int x)
 {
