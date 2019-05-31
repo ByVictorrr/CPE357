6ea608a001650fb4db372a6e5e1cd4fe11905a62
Victor Delaplaine <vdelaplaines@aol.com>
2019-05-30 22:20:43 -0700

    autoadd

---
+++ hw/hw5/parseline/debugger.gdb
@@ -0,0 +1,4 @@
+#usage: gdb -x debugger.gdb ./pro
+b 92
+b 102
+tui enable
--- hw/hw5/parseline/parseline.c
+++ hw/hw5/parseline/parseline.c
@@ -1,41 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
+#include <signal.h>
+#include "readLongLine.h"
+#define WORD_MAX 10
+#define PROGV_MAX 10
+#define PROGS_MAX 10
 
-#include "read_long_line.h"
-#define WORD_MAX 1000
-#define PROGV_MAX 1000
-#define PROGS_MAX 1000
+/*================Debuggin fucntions=============== */
+void handle_SEGFAULT(int signo){
+	if(signo == SIGSEGV){
+	}
+	exit(EXIT_FAILURE);
+}
+void print_progv(char **progv, int size){
+	int i;
+	char empty[PROGV_MAX] = {'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+	for(i = 0; i< size; i++)
+		if(strcmp(progv[i], empty) != 0)
+			printf("progv[%d] = %s\n", i, progv[i]);
+}
 
-
+/* ================================================ */
 
 /*==============Utility Functions====================*/
-void safe_malloc_3(char ****p, int size)
+void init_word_buff(char **p, int word_size)
 {
-	if( ***p = (char ***)malloc(sizeof(char)*size) == NULL)
+	if( (*p = (char *)malloc(sizeof(char)*word_size)) == NULL)
 	{
 		perror("malloc err");
-		exit(EXIT_FAILURE):
+		exit(EXIT_FAILURE);
 	}
+	memset(*p, '\0', word_size);
+}
+void init_progv_buff(char ***p, int progv_size, int word_size)
+{
+	int i;
+	if( (*p = (char **)malloc(sizeof(char*)*progv_size)) == NULL)
+	{
+		perror("malloc err");
+		exit(EXIT_FAILURE);
+	}
+	for(i = 0; i < progv_size; i++){
+		init_word_buff(&p[0][i], word_size);
+	}
+}
+void init_progs_buff(char ****p, int progs_size, int progv_size, int word_size)
+{
+	int i;
+	if( (*p = (char ***)malloc(sizeof(char**)*progs_size)) == NULL)
+	{
+		perror("malloc err");
+		exit(EXIT_FAILURE);
+	}
+	for(i = 0; i < progs_size; i++){
+		init_progv_buff(&p[0][i], progv_size, word_size);
+	}
+
 }
 
-void safe_malloc_2(char ***p, int size)
-{
-	if( **p = (char **)malloc(sizeof(char)*size) == NULL)
-	{
-		perror("malloc err");
-		exit(EXIT_FAILURE):
-	}
-}
 
-void safe_malloc_1(char **p, int size)
-{
-	if( *p = (char *)malloc(sizeof(char)*size) == NULL)
-	{
-		perror("malloc err");
-		exit(EXIT_FAILURE):
-	}
-}
 /*===================================================*/
 
 
@@ -52,29 +76,32 @@ void safe_malloc_1(char **p, int size)
 /*Treat > and < part of the the progn */
 /*   */
 char ***get_progs_with_options(char *line){
+
 	char ***progs_buff, **progv_buff, *word_buff;
 	int i, word_ptr, progv_ptr, progs_ptr;
 	/* i - line ptr */
 
 	/*Step 0 - initalize mem for all buffers */
-	safe_malloc_1(&word_buff, WORD_MAX);
-	safe_malloc_2(&progv_buff, PROGV_MAX);
-	safe_malloc_3(&progs_buff, PROGS_MAX);
-
+	init_word_buff(&word_buff, WORD_MAX);
+	init_progv_buff(&progv_buff, PROGV_MAX, WORD_MAX);
+	init_progs_buff(&progs_buff, PROGS_MAX, PROGV_MAX, WORD_MAX);
 
 	for( i = 0, word_ptr = 0, progv_ptr = 0, progs_ptr = 0; line[i] != '\0'; i++){
 			/*Case 1- new word*/
 			if(line[i] == ' ' && line[i+1] != '|'){
 				/*Step 1 - add this word to the progv*/
-				strcpy(progv[progv_ptr], word_buff);
-				progv_buff++;
+				strcpy(progv_buff[progv_ptr], word_buff);
+				print_progv(progv_buff, PROGV_MAX);
+				progv_ptr++;
 				/*========reset word============= */
-				memset(word_ptr,'\0', WORD_MAX);
+				memset(word_buff,'\0', WORD_MAX);
 				word_ptr = 0;
 				/*=============================== */
 			/*Case 2 - new prog */
 			}else if(line[i] == ' ' && line[i+1] == '|'){
-				strcpy(progs_buff[progs_ptr], progv_buff);
+				strcpy(progv_buff[progv_ptr], word_buff);
+				print_progv(progv_buff, PROGV_MAX);
+				memcpy(progs_buff[progs_ptr], progv_buff, PROGV_MAX);
 				progs_ptr++;
 				/*=====reset word and progv =======*/
 				memset(word_buff, '\0', WORD_MAX);
@@ -88,14 +115,23 @@ char ***get_progs_with_options(char *line){
 				word_ptr++;
 			}
 	}
+	return progs_buff;
 }
 
 int main(int argc, char **argv)
 {
 	char ***progs;
-	char *line = read_long_line(STDIN_FILENO);
+	int fdTest;
+	char *line;
 
+	fdTest = open("inputs/test1", O_RDWR);
+	line = read_long_line(fdTest);
+
+	if(signal(SIGSEGV, handle_SEGFAULT) == SIG_ERR) {
+	    fputs("An error occurred while setting a signal handler.\n", stderr);
+		exit(EXIT_FAILURE);
+	}
 	progs=get_progs_with_options(line);
 
-
+	return 0;
 }
