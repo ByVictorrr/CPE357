6ffe7ecf0086d4a881056c52a6f6a3f440db1bda
Victor Delaplaine <vdelaplainess@gmail.com>
2019-06-10 01:05:37 -0700

    Update readLongLine.c

--- hw/hw6/mush/readLongLine.c
+++ hw/hw6/mush/readLongLine.c
@@ -1,12 +1,12 @@
 #include "readLongLine.h"
 
 
-char *read_long_line(int inFd)
+char *read_long_line(FILE *stream, int interactive)
 {
 	int index = 0;
 	char *pbuff;
 	int sizebuff = MAXCHAR;
-	char c; /*temp is for moving allong pbuff*/
+	int c; /*temp is for moving allong pbuff*/
 
 	/*initalize current size of buffer*/	
 	int onebuff = 0;
@@ -15,20 +15,16 @@ char *read_long_line(int inFd)
 	pbuff = (char*)calloc(MAXCHAR, sizeof(char));
 	memset(pbuff, '\0', MAXCHAR);
 	
-	write(STDOUT_FILENO, "8-P: ", strlen("8-P: "));
-	while((read(inFd, &c, sizeof(char))) > 0)
-	{
-
-		if(c=='\n')
-			return pbuff;
-	   /* print_binary(c);
-
-	    printf(" above is %c \n", c);
-	    */
-		/*case 3: store value of c in buffer */
+	if(interactive == INTERACTIVE){
+		fwrite("8-P: ",1,strlen("8-P: "), stdout);
+	}
+	while((c = getc(stream)) != EOF){
+		/*Case 1 - interactive cmd line mode read until "\n"*/
+		if(interactive == INTERACTIVE){
+			if(c == '\n')
+				return pbuff;
+		}
 		pbuff[index++] = c;
-		/*case 4: check if an overflow occured in buffer same buff
-		 *		count number of overflows in buff */
 		if ( index >= MAXCHAR-1 )
 		{
 			/*Size alloationa are alwaysgoing to be mutliples os MAXCHAr*/
@@ -36,6 +32,8 @@ char *read_long_line(int inFd)
 			pbuff = (char*)realloc(pbuff,sizebuff);
 		}
 	}
+	if(c == EOF && interactive == INTERACTIVE)
+		exit(EXIT_FAILURE);
 
 	return pbuff;
 }
