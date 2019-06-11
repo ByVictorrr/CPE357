1f60826ccf3a67fcec873198710a6ecb8bbb8125
Victor Delaplaine <vdelaplainess@gmail.com>
2019-06-10 22:19:46 -0700

    Update mypwd.c

--- labs/lab6/part1/mypwd.c
+++ labs/lab6/part1/mypwd.c
@@ -1,120 +1,101 @@
+/*
+ * mypwd.c
+ * Copyright (C) 2019 victor <victor@TheShell>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#define CWD 0
 
-#define MAX 4096
 
-void checkDir(DIR **directory);
+void transverse(ino_t prev_ino, ino_t root_ino, ino_t cwd_ino, char *pwd, int *pwd_ptr){
 
-int setInoDev(struct stat *buf, ino_t* ino, dev_t* dev);
+	DIR *dp;
+	struct dirent *entry;
+	struct stat buff, buff_cwd;
 
-void lookForDir(DIR** directory, struct dirent** dir, 
-char (*path)[MAX], ino_t* ino, dev_t* dev);
+	if(*pwd_ptr > PATH_MAX){
+		perror("path name to long");
+		exit(EXIT_FAILURE);
+	}
+	/*Base case prev_ino == root_ino */
+	if(prev_ino == root_ino){
+		strcpy(pwd, "/");
+		*pwd_ptr = 1;
+		return;
+	}
+	/*Case 1 - in current directory  */
+	if(prev_ino == CWD){
+		/*Step 1 - get current current dir i_node number  */
+		lstat(".", &buff);
+		/*Step 2 - get inode  */
+		prev_ino = buff.st_ino;
+		/*Step 3- change dir to parent */
+		if (chdir("..") < 0){
+			perror("..");
+			exit(EXIT_FAILURE);
+		}
+	}
+	/*Case 2 - get name of current directory*/
+	if((dp = opendir(".")) == NULL){
+		perror(".");
+		exit(EXIT_FAILURE);
+	}
+	/*Case 3 - go through current directory */
+	while((entry = readdir(dp)) != NULL){
+		/*Step 1 - get stats of the the entry*/
+		if(lstat(entry->d_name, &buff) < 0){
+			perror(entry->d_name);
+			exit(EXIT_FAILURE);
+		}
+		/*Step 2 - if the entry is a directory and has the inode number of prev_ino get that name*/
+		if(S_ISDIR(buff.st_mode) && prev_ino == buff.st_ino){
+			/*Step 3 - get st_ino of . and call function*/
+			lstat(".", &buff_cwd);
+			chdir("..");
+			transverse(buff_cwd.st_ino, root_ino, cwd_ino, pwd, pwd_ptr);
+			/*Step 4 - strcat entry->d_name to pwd */
+			strcat(pwd, entry->d_name);
+			if(cwd_ino != prev_ino)
+				strcat(pwd, "/");
+			
+			*pwd_ptr = strlen(pwd);
+			closedir(dp);
+			return;
+		}
 
-void strChange(char (*str1)[MAX], char (*str2)[256]);
+	}
 
-void printPath(char *path);
-
-int main(){
-
-   struct stat buf;
-   struct dirent* dir;
-   DIR* directory = NULL;
-   char path[MAX];
-   dev_t dev;
-   ino_t ino;
-   ino = dev = 0;
-
-   while(1){
-
-      if(setInoDev(&buf, &ino, &dev)){
-         printPath(path);
-         return EXIT_SUCCESS;
-      }
-
-      checkDir(&directory);
-
-      lookForDir(&directory, &dir, &path, &ino, &dev);
-
-   }
-
-   return EXIT_SUCCESS;
 }
 
-int setInoDev(struct stat *buf, ino_t* ino, dev_t* dev){
-   ino_t oldIno = *ino; 
-   dev_t oldDev = *dev;
 
-   if(stat(".", buf) < 0){
-      perror(NULL);
-      exit(EXIT_FAILURE);
-   }
 
-   if((oldIno == buf->st_ino) && (oldDev == buf->st_dev))
-      return 1;
-   
-   *ino = buf->st_ino;
-   *dev = buf->st_dev;
 
-   return 0;
+int main(int argc, char **argv)
+{
+	ino_t prev = CWD; /*We cant get the name in the current directory so we have to use the ino_t to compare if thats the name in the parrent  */
+	char pwd[PATH_MAX] = {'\0'};
+	int pwd_ptr = 0;
+	struct stat root_buff, cwd_buff;
+
+	
+	if(lstat("/", &root_buff) < 0){
+		perror("/");
+		exit(EXIT_FAILURE);
+	}
+
+	if(lstat(".", &cwd_buff) < 0){
+		perror("/");
+		exit(EXIT_FAILURE);
+	}
+	transverse(prev, root_buff.st_ino, cwd_buff.st_ino, pwd, &pwd_ptr);
+
+	printf("pwd = %s ", pwd);
+
+	return 0;
 }
-
-void checkDir(DIR **directory){
-   if(chdir("..") < 0){
-      perror(NULL);
-      exit(EXIT_FAILURE);
-   }
-
-   if((*directory = opendir(".")) == NULL){
-      perror(NULL);
-      exit(EXIT_FAILURE);
-   }
-}
-
-void lookForDir(DIR** directory, struct dirent** dir, 
-   char (*path)[MAX], ino_t* ino, dev_t* dev){
-   
-   struct stat temp;
-   
-   while((*dir = readdir(*directory)) != NULL){
-      stat((*dir)->d_name, &temp);
-      if(temp.st_dev == (*dev) && temp.st_ino == (*ino))
-         strChange(path, &((*dir)->d_name));
-   }
-
-   closedir(*directory);
-}
-
-void strChange(char (*str1)[MAX], char (*str2)[256]){
-   char buffer[MAX];
-   int i, len1, len2;
-   len1 = strlen(*str1);
-   len2 = strlen(*str2);
-
-   buffer[0] = '/';
-
-   for(i = 1; i < len2 + 1; i++)
-      buffer[i] = (*str2)[i-1];
-
-   len2++;
-
-   for(i = len1; i >= 0; i--)
-      (*str1)[i + len2] = (*str1)[i];
-
-   for(i = 0; i < len2; i++)
-      (*str1)[i] = buffer[i]; 
-}
-
-void printPath(char *path){
-   int i;
-   for(i = 5; i < strlen(path); i++)
-      printf("%c", path[i]);
-   
-   printf("\n");
-}
-
